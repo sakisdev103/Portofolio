d4cac54456579e643d61b4fd73f30a4338f8ac56
Your Name <sakistr69@gmail.com>
2024-05-07 22:54:21 +0300

    Updated the Weather Application

--- src/data.js
+++ src/data.js
@@ -1,8 +1,6 @@
 import myCompany from "./images/my-company.png";
 import grocery from "./images/Grocery Bud.png";
 import expenses from "./images/Expenses.png";
-import movies from "./images/Movies.png";
-import camps from "./images/Camps.png";
 import gym2 from "./images/Gym2.png";
 import car from "./images/car-rental.png";
 import timeseries from "./images/Timeseries.png";
@@ -24,11 +22,11 @@ export const projects = [
   {
     id: "2",
     name: "Weather",
-    link: "https://thanosdev.com/AgroApps_Weather_App/",
+    link: "https://thanosdev.com/Weather/",
     img: weather,
     description:
-      "A simple, modern, responsive and elegant weather application that displays weather information about a specific location(Thessaloniki). The data is provided in api folder and more specific in weatherAPI.json file. The user can select to see information about current weather, today's weather or a date in the near future.",
-    technologies: ["HTML", "BOOTSTRAP", "JAVASCRIPT", "REACT"],
+      "A simple, modern, responsive and elegant weather application that displays weather information based on searched city.",
+    technologies: ["HTML", "MATERIAL UI", "JAVASCRIPT", "REACT"],
   },
   {
     id: "3",
@@ -68,6 +66,15 @@ export const projects = [
   },
   {
     id: "7",
+    name: "Grocery",
+    link: "https://thanosdev.com/Grocery_App/",
+    img: grocery,
+    description:
+      "Developed using advanced javascript, despite the simplicity of the design. The user can add products to his list, delete or edit them.",
+    technologies: ["HTML", "CSS", "JAVASCRIPT"],
+  },
+  {
+    id: "8",
     name: "Business Site",
     link: "https://thanosdev.com/Business_Site/",
     img: myCompany,
@@ -76,7 +83,7 @@ export const projects = [
     technologies: ["HTML", "CSS", "JAVASCRIPT"],
   },
   {
-    id: "8",
+    id: "9",
     name: "Gym Tracker",
     link: "https://thanosdev.com/Gym_Tracker/",
     img: gym2,
@@ -85,39 +92,12 @@ export const projects = [
     technologies: ["HTML", "CSS", "JAVASCRIPT", "PHP", "MYSQL"],
   },
   {
-    id: "9",
+    id: "10",
     name: "Car Rental",
     link: "https://thanosdev.com/Car_Rental/",
     img: car,
     description:
       "The design is very simple, althouth it is fully responsive and mobile friendly. The project includes login / registration page, dynamic orders based on client id, dynamic product display based on availability and fully scalable admin panel which includes diagraph, add products or edit / delete them from the list. In the admin panel , the user can also see the contact list.",
     technologies: ["HTML", "CSS", "JAVASCRIPT", "PHP", "MYSQL"],
-  },
-  {
-    id: "10",
-    name: "Movies",
-    link: "https://thanosdev.com/Movies_App/",
-    img: movies,
-    description:
-      "Developed with REACTJS. The user can search for movies / series and data will be displayed on his screen. The application is fully responsive as well as mobile friendly.",
-    technologies: ["HTML", "CSS", "JAVASCRIPT", "REACT"],
-  },
-  {
-    id: "11",
-    name: "Camps",
-    link: "https://thanosdev.com/Camps-App/",
-    img: camps,
-    description:
-      "In this application the user can see a wide variety of camps, search using the search bar, add his own, select the camp of his choise to see more information about it and read comments from other users, the user can also add his own comments in the camp of his choise.",
-    technologies: ["HTML", "CSS", "JAVASCRIPT", "REACT"],
-  },
-  {
-    id: "12",
-    name: "Grocery",
-    link: "https://thanosdev.com/Grocery_App/",
-    img: grocery,
-    description:
-      "Developed using advanced javascript, despite the simplicity of the design. The user can add products to his list, delete or edit them.",
-    technologies: ["HTML", "CSS", "JAVASCRIPT"],
   },
 ];
