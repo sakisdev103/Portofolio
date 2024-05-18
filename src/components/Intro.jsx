246cfed74b6928089eb0c950bbbf42e0c898b1b4
Your Name <sakistr69@gmail.com>
2024-05-18 13:34:08 +0300

    Small changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ const Intro = () => {
   return (
     <>
       <span>Hi, my name is</span>
-      <h1>Trentsios Athanasios.</h1>
+      <h1>Thanos Trentsios.</h1>
       <h2>I build things for the web.</h2>
       <p>
         I’m a software developer specializing in front end and occasionally in
